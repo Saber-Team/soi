e57c8cf6c6e2455868d3153d8332f22a41d59e63
AceMood <zmike86@163.com>
2014-12-13 20:58:39 +0800

    conf file

--- config/soi.conf.js
+++ config/soi.conf.js
@@ -27,7 +27,7 @@ module.exports = {
 
     // which dir to be relative, defaults to where soi.conf.js located;
     // all path should be calculated based on this directory
-    base_dir        : './',
+    base_dir        : __dirname + '/',
 
     // after build all files placed, if null value, all files would generated
     // be with the original file, but I highly recommend need a dist directory
