78adf6fb81bda87f1bb06a1bc296824ef88326ea
AceMood <zmike86@163.com>
2014-12-19 16:59:39 +0800

    add

--- tests/resolve_css_deps_calc/cssDeps-Spec.js
+++ tests/resolve_css_deps_calc/cssDeps-Spec.js
@@ -5,6 +5,41 @@ var parser = require('../../lib/resolver/cssParser');
 
 describe('CSS dependency calculate', function() {
 
+    var entry_point = __dirname + '/css/a.css';
 
+    function swift(str) {
+        return str.replace(/\\/g, '/');
+    }
+
+    it('#order', function() {
+        var arr = parser.parse({
+            css_entry_point: entry_point,
+            encoding: 'utf8'
+        });
+
+        expect(arr.length).to.equal(3);
+        expect(swift(arr[0])).to.equal(swift(__dirname + '/css/inner/c.css'));
+        expect(swift(arr[1])).to.equal(swift(__dirname + '/css/b.css'));
+        expect(swift(arr[2])).to.equal(swift(__dirname + '/css/a.css'));
+    });
+
+    it('#unique', function() {
+        var arr = parser.parse({
+            css_entry_point: entry_point,
+            encoding: 'utf8'
+        });
+
+        expect(arr.length).to.equal(3);
+    });
+
+    it('#circle reference', function() {
+        var arr = parser.parse({
+            css_entry_point: entry_point,
+            encoding: 'utf8'
+        });
+
+        expect(arr.length).to.equal(3);
+
+    });
 
 });
