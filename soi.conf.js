92a403891138757d2301499ef4fd1cfcef711f0f
AceMood <zmike86@163.com>
2014-12-22 18:35:28 +0800

    ref

--- soi.conf.js
+++ soi.conf.js
@@ -7,77 +7,105 @@
 
 module.exports = {
 
-    // read-in stream should be what encoding
-    encoding        : 'utf8',
+  // read-in stream should be what encoding
+  encoding        : 'utf8',
 
-    // what kind of files should be ignored
-    // new RegExp('\\.(' + js_config.ignoreFileExt.join('|') + ')$', 'i')
-    ignoreFileExt   : ['md', 'json', 'txt', 'tpl', 'tmpl', 'java', 'py', 'rb', 'cs', 'h', 'rar', 'zip'],
+  // what kind of files should be ignored
+  // new RegExp('\\.(' + js_config.ignoreFileExt.join('|') + ')$', 'i')
+  ignoreFileExt   : ['md', 'json', 'txt', 'tpl', 'tmpl', 'java', 'py', 'rb', 'cs', 'h', 'rar', 'zip'],
 
-    // debug mode
-    debug           : true,
+  // debug mode
+  debug           : true,
 
-    // directories that would not be calculated
-    preserved       : {
-        'imgs'      : true,
-        'build'     : true,
-        'dist'      : true,
-        'templates' : true,
-        'demos'     : true
-    },
+  // directories that would not be calculated
+  preserved       : {
+    'imgs'        : true,
+    'build'       : true,
+    'dist'        : true,
+    'templates'   : true,
+    'demos'       : true
+  },
 
-    // which dir to be relative, defaults to where soi.conf.js located;
-    // all path should be calculated based on this directory
-    base_dir        : __dirname + '/',
+  // which dir to be relative, defaults to where soi.conf.js located;
+  // all path should be calculated based on this directory
+  base_dir        : __dirname + '/',
 
-    // after build all files placed, if null value, all files would generated
-    // be with the original file, but I highly recommend need a dist directory
-    dist_dir        : './demo/dist/',
+  // after build all files placed, if null value, all files would generated
+  // be with the original file, but I highly recommend need a dist directory
+  dist_dir        : './demo/dist/',
 
-    // dist file relative to dist_dir
-    dist_css_file   : 'css/build.css',
+  // dist file relative to dist_dir
+  dist_css_file   : 'css/build.css',
 
-    // dist file relative to dist_dir
-    dist_js_file    : 'js/build.js',
+  // dist file relative to dist_dir
+  dist_js_file    : 'js/build.js',
 
-    // js module loader
-    module_loader   :  './demo/lib/require.js',
+  // js module loader
+  module_loader   :  './demo/lib/require.js',
 
-    // start css file as the entry point
-    // *I HIGHLY RECOMMEND* bundle all css files into one
-    // So it's only need to provide single entry point
-    css_entry_point : './demo/assets/css/test.css',
+  // start css file as the entry point
+  // *I HIGHLY RECOMMEND* bundle all css files into one
+  // So it's only need to provide single entry point
+  css_entry_point : './demo/assets/css/test.css',
 
-    // logic start file or async load modules entry file
-    input_file      :  [
-        {
-            path    : './demo/assets/js/app.js',
-            defer   : false
-        }
-    ],
-
-    // If debug set to true, we around all js files' path with script html tag
-    // and write it to this file, which can be paste into your template or html file
-    // for source debugging (or you can use source map)
-    output_file     : './output/temp.txt',
-
-    // If debug set to true, output_file_base could be work together with output_file
-    // it's the directory where template located
-    output_base     : './demo/',
-
-    // Manually added files
-    // 需要遍历的目录，这些目录的所有文件都会被计算
-    files           : {
-        css         : [
-            './demo/assets/css/'
-        ],
-        js          : [
-            './demo/lib/',
-            './demo/assets/js/'
-        ],
-        img         : [
-            './demo/assets/img/'
-        ]
+  // logic start file or async load modules entry file
+  input_file      :  [
+    {
+      path    : './demo/assets/js/app.js',
+      defer   : false
     }
+  ],
+
+  // If debug set to true, we around all js files' path with script html tag
+  // and write it to this file, which can be paste into your template or html file
+  // for source debugging (or you can use source map)
+  output_file     : './output/temp.txt',
+
+  // If debug set to true, output_file_base could be work together with output_file
+  // it's the directory where template located
+  output_base     : './demo/',
+
+  // Manually added files
+  // 需要遍历的目录，这些目录的所有文件都会被计算
+  files           : {
+    css           : [
+      './demo/assets/css/'
+    ],
+    js            : [
+      './demo/lib/',
+      './demo/assets/js/'
+    ],
+    img           : [
+      './demo/assets/img/'
+    ]
+  },
+
+  // pack config
+  // *input: start module from which to calculate
+  // *directory: all the needed files located
+  // *defer: whether the package will be loaded on demand or first view
+  //    false is needed on first view, true means will be loaded on demand.
+  // *dist: concat file name(will be change suffix with sha1 hash sum)
+  // *dist_dir: where concat file located
+  bundle          : {
+    css           : [
+      {
+        input     : './demo/assets/css/test.css',
+        directory : [ './demo/assets/css/' ],
+        defer     : false,
+        dist      : 'build.css',
+        dist_dir  : './demo/dist/css/'
+      }
+    ],
+    js            : [
+      {
+        input     : './demo/assets/js/app.js',
+        directory : [ './demo/lib/', './demo/assets/js/' ],
+        defer     : false,
+        dist      : 'build.js',
+        dist_dir  : './demo/dist/js/'
+      }
+    ]
+  }
 
 };
