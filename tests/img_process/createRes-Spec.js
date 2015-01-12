ebbdc49e1b0658f87a3b4bd9395719a927ef392b
AceMood <zmike86@163.com>
2015-01-12 21:59:16 +0800

    add rimraf module

--- tests/img_process/createRes-Spec.js
+++ tests/img_process/createRes-Spec.js
@@ -1,7 +1,9 @@
 var chai = require('chai');
 var expect = chai.expect;
 var path = require('path');
+var fs = require('fs');
 
+var rimraf = require('rimraf');
 var cli = require('../../lib/cli');
 var utils = require('../../lib/utils');
 var ProcesssorFactory = require('../../lib/processor/factory');
@@ -22,23 +24,37 @@ describe('create image resources', function() {
             defer     : false,
             dist_file : null,
             dist_dir  : './dist/'
+          },
+          {
+            input     : null,
+            files     : [ './img/c.png' ],
+            exclude   : {},
+            defer     : false,
+            dist_file : null,
+            dist_dir  : './dist/'
           }
         ]
       }
     };
     // 需要先针对路径处理cwd问题
     cli.processConfigOptions();
+
+    ProcesssorFactory
+      .getInstance('img')
+      .traverse();
   });
 
   after(function() {
     global.SOI_CONFIG = null;
+    rimraf(path.join(__dirname, 'dist/inner/'), function(err) {
+      //debugger;
+    });
+    rimraf(path.join(__dirname, 'dist/'), function(err) {
+      //debugger;
+    });
   });
 
   it('a.png in img', function() {
-    ProcesssorFactory
-      .getInstance('img')
-      .traverse();
-
     var id = utils.normalizeSysPath(
       path.join(SOI_CONFIG.base_dir + './img/a.png'));
     var img = ResourceTable.getResource('img', id);
@@ -54,10 +70,6 @@ describe('create image resources', function() {
   });
 
   it('b.png in img/inner/', function() {
-    ProcesssorFactory
-      .getInstance('img')
-      .traverse();
-
     var id = utils.normalizeSysPath(
       path.join(SOI_CONFIG.base_dir + './img/inner/b.png'));
     var img = ResourceTable.getResource('img', id);
@@ -65,6 +77,21 @@ describe('create image resources', function() {
     expect(img).to.not.equal(undefined);
     expect(img.path).to.equal(utils.normalizeSysPath(
       path.join(SOI_CONFIG.base_dir + './img/inner/b.png')
+    ));
+    expect(img.type).to.equal('img');
+    expect(img.origin).to.equal(utils.normalizeSysPath(
+      path.join(SOI_CONFIG.base_dir + './img/')
+    ));
+  });
+
+  it('c.png in img', function() {
+    var id = utils.normalizeSysPath(
+      path.join(SOI_CONFIG.base_dir + './img/c.png'));
+    var img = ResourceTable.getResource('img', id);
+
+    expect(img).to.not.equal(undefined);
+    expect(img.path).to.equal(utils.normalizeSysPath(
+      path.join(SOI_CONFIG.base_dir + './img/c.png')
     ));
     expect(img.type).to.equal('img');
     expect(img.origin).to.equal(utils.normalizeSysPath(
