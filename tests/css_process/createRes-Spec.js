87546f836756ea2e046f365cfef339a491b781c7
AceMood <zmike86@163.com>
2015-01-13 12:51:15 +0800

    css process testcases

---
+++ tests/css_process/createRes-Spec.js
@@ -0,0 +1,98 @@
+var chai = require('chai');
+var expect = chai.expect;
+var path = require('path');
+var fs = require('fs');
+
+var rimraf = require('rimraf');
+var cli = require('../../lib/cli');
+var utils = require('../../lib/utils');
+var ProcesssorFactory = require('../../lib/processor/factory');
+var ResourceTable = require('../../lib/resource/table');
+
+describe('create image resources', function() {
+
+  before(function() {
+    global.SOI_CONFIG = {
+      encoding : 'utf8',
+      base_dir : __dirname + '/',
+      bundles: {
+        css: [
+          {
+            input     : './css/main.css',
+            files     : null,
+            exclude   : {},
+            defer     : false,
+            dist_file : 'build.css',
+            dist_dir  : './dist/'
+          },
+          {
+            input     : null,
+            files     : ['./css/main.css'],
+            exclude   : {},
+            defer     : false,
+            dist_file : 'bundle.css',
+            dist_dir  : './dist/'
+          }
+        ]
+      }
+    };
+    // 需要先针对路径处理cwd问题
+    cli.processConfigOptions();
+    cli.parseBundlesOptions();
+    cli.resolveFiles();
+
+  });
+
+  after(function() {
+    global.SOI_CONFIG = null;
+    rimraf.sync(path.join(__dirname, 'dist/'), function(err) {
+      //debugger;
+    });
+  });
+
+  it('a.png in img', function() {
+    var id = utils.normalizeSysPath(
+      path.join(SOI_CONFIG.base_dir + './img/a.png'));
+    var img = ResourceTable.getResource('img', id);
+
+    expect(img).to.not.equal(undefined);
+    expect(img.path).to.equal(utils.normalizeSysPath(
+      path.join(SOI_CONFIG.base_dir + './img/a.png')
+    ));
+    expect(img.type).to.equal('img');
+    expect(img.origin).to.equal(utils.normalizeSysPath(
+      path.join(SOI_CONFIG.base_dir + './img/')
+    ));
+  });
+
+  it('b.png in img/inner/', function() {
+    var id = utils.normalizeSysPath(
+      path.join(SOI_CONFIG.base_dir + './img/inner/b.png'));
+    var img = ResourceTable.getResource('img', id);
+
+    expect(img).to.not.equal(undefined);
+    expect(img.path).to.equal(utils.normalizeSysPath(
+      path.join(SOI_CONFIG.base_dir + './img/inner/b.png')
+    ));
+    expect(img.type).to.equal('img');
+    expect(img.origin).to.equal(utils.normalizeSysPath(
+      path.join(SOI_CONFIG.base_dir + './img/')
+    ));
+  });
+
+  it('c.png in img', function() {
+    var id = utils.normalizeSysPath(
+      path.join(SOI_CONFIG.base_dir + './img/c.png'));
+    var img = ResourceTable.getResource('img', id);
+
+    expect(img).to.not.equal(undefined);
+    expect(img.path).to.equal(utils.normalizeSysPath(
+      path.join(SOI_CONFIG.base_dir + './img/c.png')
+    ));
+    expect(img.type).to.equal('img');
+    expect(img.origin).to.equal(utils.normalizeSysPath(
+      path.join(SOI_CONFIG.base_dir + './img/')
+    ));
+  });
+
+});
