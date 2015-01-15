17a0c17b561fd7c5f062c96617163b5838901dd3
AceMood <zmike86@163.com>
2015-01-15 18:28:59 +0800

    add

--- tests/css_resolve/resolve-Spec.js
+++ tests/css_resolve/resolve-Spec.js
@@ -88,9 +88,9 @@ describe('css resolve cases', function() {
 
   after(function() {
     global.SOI_CONFIG = null;
-    //rimraf.sync(path.join(__dirname, 'dist/'), function(err) {
+    rimraf.sync(path.join(__dirname, 'dist/'), function(err) {
       //debugger;
-    //});
+    });
   });
 
   it('#background image', function() {
@@ -123,4 +123,93 @@ describe('css resolve cases', function() {
       '#bgi6{background:url("a_671eaeda.png"), url("inner/b_671eaeda.png");}');
   });
 
+  it('#border image', function() {
+    var id = utils.normalizeSysPath(
+      path.join(SOI_CONFIG.base_dir + './css/bi.css'));
+    var css_a = ResourceTable.getResource('css', id);
+
+    expect(css_a).to.not.equal(undefined);
+    expect(css_a.path).to.equal(utils.normalizeSysPath(
+      path.join(SOI_CONFIG.base_dir + './css/bi.css')
+    ));
+    expect(css_a.type).to.equal('css');
+    expect(css_a.origin).to.equal(null);
+
+    var rsc = ResourceTable.getPackageByPath('css', id);
+
+    expect(rsc).to.not.equal(undefined);
+    expect(rsc).to.have.property('files').with.length(1);
+    expect(fs.existsSync(rsc.dist_file)).to.equal(true);
+
+    var content = utils.readFile(rsc.dist_file, {
+      encoding: 'utf8'
+    });
+    expect(content).to.equal('#bi0{-webkit-border-image:url(a_671eaeda.png) 30 30 round;' +
+      '-o-border-image:url(a_671eaeda.png) 30 30 round;' +
+      'border-image:url(a_671eaeda.png) 30 30 round;}' +
+      '#bi1{-webkit-border-image:url("a_671eaeda.png") 30 30 round;' +
+      '-o-border-image:url("a_671eaeda.png") 30 30 round;' +
+      'border-image:url("a_671eaeda.png") 30 30 round;}' +
+      '#bi2{-webkit-border-image:url(a_671eaeda.png) 30 30 stretch;' +
+      '-o-border-image:url(a_671eaeda.png) 30 30 stretch;' +
+      'border-image:url(a_671eaeda.png) 30 30 stretch;}');
+  });
+
+  it('#behavior htc', function() {
+    var id = utils.normalizeSysPath(
+      path.join(SOI_CONFIG.base_dir + './css/htc.css'));
+    var css_a = ResourceTable.getResource('css', id);
+
+    expect(css_a).to.not.equal(undefined);
+    expect(css_a.path).to.equal(utils.normalizeSysPath(
+      path.join(SOI_CONFIG.base_dir + './css/htc.css')
+    ));
+    expect(css_a.type).to.equal('css');
+    expect(css_a.origin).to.equal(null);
+
+    var rsc = ResourceTable.getPackageByPath('css', id);
+
+    expect(rsc).to.not.equal(undefined);
+    expect(rsc).to.have.property('files').with.length(1);
+    expect(fs.existsSync(rsc.dist_file)).to.equal(true);
+
+    var content = utils.readFile(rsc.dist_file, {
+      encoding: 'utf8'
+    });
+    expect(content).to.equal('#htc0{behavior:url(ie_da8c6a4f.htc);}' +
+      '#htc1{behavior:url(ie_da8c6a4f.htc) url(ie0_da8c6a4f.htc);}');
+  });
+
+  it('#ie filter', function() {
+    var id = utils.normalizeSysPath(
+      path.join(SOI_CONFIG.base_dir + './css/ie6.css'));
+    var css_a = ResourceTable.getResource('css', id);
+
+    expect(css_a).to.not.equal(undefined);
+    expect(css_a.path).to.equal(utils.normalizeSysPath(
+      path.join(SOI_CONFIG.base_dir + './css/ie6.css')
+    ));
+    expect(css_a.type).to.equal('css');
+    expect(css_a.origin).to.equal(null);
+
+    var rsc = ResourceTable.getPackageByPath('css', id);
+
+    expect(rsc).to.not.equal(undefined);
+    expect(rsc).to.have.property('files').with.length(1);
+    expect(fs.existsSync(rsc.dist_file)).to.equal(true);
+
+    var content = utils.readFile(rsc.dist_file, {
+      encoding: 'utf8'
+    });
+    expect(content).to.equal('#filter0{background:none;' +
+      'filter:progid:DXImageTransform.Microsoft.AlphaImageLoader(src="dist/inner/b_671eaeda.png");}' +
+      '#filter1{background:none;' +
+      'filter:progid:DXImageTransform.Microsoft.AlphaImageLoader(src=dist/inner/b_671eaeda.png);}' +
+      '#filter2{background:none;' +
+      '_filter:progid:DXImageTransform.Microsoft.AlphaImageLoader("src=dist/inner/b_671eaeda.png");}' +
+      '#filter3{background:none;' +
+      '_filter:progid:DXImageTransform.Microsoft.AlphaImageLoader(src=dist/inner/b_671eaeda.png);}');
+  });
+
+  
 });
