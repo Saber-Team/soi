35d9abcbfa84b748cc6d85a64a6e4bf37a26ef0d
AceMood <zmike86@163.com>
2015-06-20 17:14:38 +0800

    dev

--- samples/release/soi-conf.js
+++ samples/release/soi-conf.js
@@ -1,48 +1,56 @@
 /**
  * @fileoverview release的配置文件样例
- * @author AceMood
- * @email zmike86@gmail.com
  */
 
 'use strict';
 
 soi.config.extend({
   release: {
-    // 这个属性非常重要。它的出现有几点意义，
+    // virtualRootDir (vrd) 这个属性非常重要。它的出现有几点意义，
     // 1. 它代表网络根路径，所有本地的静态资源都要相对这个目录生成path部分，从而产生绝对路径类似于
     //    `/static/scripts/lib/jQuery.js`。用到的是pack中的entrance属性。
     // 2. 之所以没有用系统的绝对路径是因为可能打包的系统各异，生成的资源表的key不好统一。
     //    在此生成的绝对路径作为最终资源表中的key存在，不会重复，且在各个系统中统一。
     // 3. 根据配置的domain，生成线上绝对路径。用到的是pack中的dist属性
-    virtualRootDir: './src/',
     prod: {
-      domain: '/assets/',
+      domain: '/',
       obscure: true,
-      scanJSInternal: false,
+      virtualRootDir: './src/',
+      distRootDir: './build/',
       pack: {
-        css: [
-          {
-            entrance  : 'static/css/main.css',
-            exclude   : [],
-            dist      : '/static/css/'
-          },
+        img: [
           {
             files     : [
-              'static/css/a.css',
-              'static/css/b.css'
+              '/static/img/*.(png|gif|jpg)'
             ],
-            exclude   : [],
-            dist      : '/static/css/'
-          }
-        ],
-        js: [
-          {
-            entrance  : 'static/js/app.js',
-            exclude   : [],
-            obscure   : true,
-            dist      : '/static/js/'
+            ignore    : [],
+            dist      : '/online/static/'
           }
         ]
+        /*,
+         css: [
+         {
+         entrance  : 'static/css/main.css',
+         ignore    : [],
+         dist      : '/static/css/'
+         },
+         {
+         files     : [
+         'static/css/a.css',
+         'static/css/b.css'
+         ],
+         ignore    : [],
+         dist      : '/static/css/'
+         }
+         ],
+         js: [
+         {
+         entrance  : 'static/js/app.js',
+         exclude   : [],
+         obscure   : true,
+         dist      : '/static/js/'
+         }
+         ]*/
       },
       replace: {
         'http://zhida.baidu.com:8080/': 'http://zhida.baidu.com',
