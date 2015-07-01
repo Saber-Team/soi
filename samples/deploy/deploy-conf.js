3785ecd302ed2d82c3f9b7c05fca97825e71076d
AceMood <zmike86@163.com>
2015-07-01 11:23:11 +0800

    dev

--- samples/deploy/deploy-conf.js
+++ samples/deploy/deploy-conf.js
@@ -7,14 +7,6 @@
 'use strict';
 
 
-var files = [
-  {
-    from: 'src/static/js/*.js',
-    to: 'static/js/'
-  }
-];
-
-
 soi.config.extend({
   // 配置节点
   deploy: {
@@ -22,10 +14,23 @@ soi.config.extend({
     // 要被复制到的目录，替换成远程绝对路径
     dist: '/Users/acemood/demo/',
     // 文件的映射, from本地目录中的文件将会被复制到to的目录中,
-    // from相对于server-conf.js文件取相对位置,
-    // to则取rootDir的相对位置.
-    // from和to支持glob形式的匹配, 也支持自己写逻辑的函数返回字符串
+    // from 相对于server-conf.js文件取相对位置,
+    // to 则取`dist`的相对位置.
+    // from 和 to 支持glob形式的匹配, 也支持自己写逻辑的函数返回字符串
     // 关于glob规范见: https://www.npmjs.com/package/glob
-    files: files
+    files: [
+      {
+        from: 'src/static/js/*.js',
+        to: 'static/js/'
+      },
+      {
+        from: 'src/static/css/*.css',
+        to: 'static/css/'
+      },
+      {
+        from: 'src/static/img/*.*',
+        to: 'static/img/'
+      }
+    ]
   }
 });
