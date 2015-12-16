bcb9f279ae68799bf4ec495eaefbeac5ed270efc
swxy <swxymile@163.com>
2015-12-16 18:35:40 +0800

    部署相关代码，初步测试可以部署成功，但每次允许时需要删除.cache文件

---
+++ samples/deploy/demo.conf.js
@@ -0,0 +1,26 @@
+/**
+ * Created by baidu on 15/12/15.
+ */
+// 配置线上路径
+soi
+    .addRule(/merchant\/img\/.*\.png$/, {
+        to: 'static/images/'
+    })
+    .addRule(/merchant\/(.*)\/.*\.js$/, {
+        to: 'static/js/'
+    });
+
+soi.deploy.task('dev',
+    {
+        receiver: 'http://cp01-zhida-mkt.epc.baidu.com:8343/receiver',
+        dir: '/home/work/webroot/soi-test',
+        cacheTo: '../build/.cache',
+        mapTo: '../build/map.json',
+        scandirs: ['.'],
+        ignorePaths: function(){},
+        watch: true,
+        cmdWrapper: {
+            usestrict: false,
+            commentdoc: ''
+        }
+    });
