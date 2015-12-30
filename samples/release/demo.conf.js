5b6d6ecdf6186913798cfd6eb5152fadb1d58672
AceMood <zmike86@163.com>
2015-12-30 12:50:45 +0800

    test for beta

--- samples/release/demo.conf.js
+++ samples/release/demo.conf.js
@@ -4,51 +4,52 @@
 // 配置线上路径
 soi
     .addRule(/merchant\/img\/.*\.png$/, {
-        to: 'static/images/'
+      to: 'static/images/'
     })
     .addRule(/merchant\/(.*)\/.*\.js$/, {
-        to: 'static/js/'
+      to: 'static/js/'
     });
 
 soi.deploy.task('dev',
     {
-        receiver: 'http://cp01-zhida-mkt.epc.baidu.com:8343/receiver',
-        dir: '/home/work/webroot/soi-test',
-        cacheTo: '../build/.cache',
-        mapTo: '../build/map.json',
-        scandirs: ['./src'],
-        ignorePaths: function(){},
-        watch: true,
-        cmdWrapper: {
-            usestrict: false,
-            commentdoc: ''
-        }
+      receiver: 'http://cp01-zhida-mkt.epc.baidu.com:8343/receiver',
+      dir: '/home/work/webroot/soi-test',
+      cacheTo: '../build/.cache',
+      mapTo: '../build/map.json',
+      scandirs: ['./src'],
+      ignorePaths: function(){},
+      watch: true,
+      cmdWrapper: {
+        usestrict: false,
+        commentdoc: ''
+      }
     });
 
 soi.release.task('dev',
     {
-        dir: './build/',
-        mapTo: '../build/map.json',
-        cacheTo: '../build/.cache',
-        scandirs: ['./src/static/javascript'],
-        ignorePaths: function(){},
-        cmdWrapper: {
-            define: '__d',
-            commentdoc: ''
-        }
+      dir: './build/',
+      mapTo: '../build/map.json',
+      cacheTo: '../build/.cache',
+      scandirs: ['src'],
+      ignorePaths: function(){},
+      cmdWrapper: {
+        define: '__d',
+        commentdoc: ''
+      }
     })
-    .addRule(/src\/static\/(.*)\//, {to : '/static/$1/'})
+    .addRule(/src\/(.*)\/.*/, {
+      to : '/static/$1/'
+    })
     .use('less')
     .use('css')
     .use('hash', {
-        algorithm: 'sha1',
-        length: 9,
-        noname: true,
-        encoding: 'base64'
+      length: 7,
+      encoding: 'hex'
     })
-    /*.use('uglify', {
-        debug: false,
-        curl: true,
-        eqeqeq: false
+    /*
+    .use('uglify', {
+      debug: false,
+      curl: true,
+      eqeqeq: false
     })*/;
 
