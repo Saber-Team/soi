c3cc9d7f9163d8a8815f4868c9bf99060d4ed963
AceMood <zmike86@163.com>
2016-02-18 21:05:15 +0800

    test rebuild

---
+++ samples/release/dist/static/pkg/40c187d.pkg.build.js
@@ -0,0 +1,5 @@
+__d("fvr+Q",function(n,f,i){f.identity="noname",f.fulfill=!1});
+__d("HOicX",function(n,t,o){function e(t){n.async(["vrcode"],function(n){n.isPast()})}var c=document.querySelector("button");document.addEventListener(c,"click",e)});
+__d("SqQ7E",function(n,t,e){var o=n("base");t.getModuleA=function(){return new o}});
+__d("vrcode",function(n,t,o){t.isPast=function(){return Boolean('<%$_REQUEST["token"]%>')}});
+__d("base",function(n,t,o){o.exports={name:"A"}});
